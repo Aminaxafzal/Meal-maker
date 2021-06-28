ed968a711d18f522963896e3e1526ab9f549a6cf
minamedia <mina.m3dia@gmail.com>
2021-06-28 13:29:19 +0100

    cleaned-up-syntax-created-new-methods

--- main.js
+++ main.js
@@ -14,5 +14,33 @@ const menu = {
      return this._courses.desserts;
       },
     
+    set appetizers(appetizers) {
+     return this._courses.appetizers = appetizers;
+    },
+    set mains(mains) {
+     return this._courses.mains = mains;
+    },
+    set dessert(dessert) {
+     return this._courses.dessert = dessert;
+    },
+    get _courses() {
+      return {
+        appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.dessert
+      };
+         },
+    addDishToCourse(courseName, dishName, dishPrice) {
+    const dish = {
+      name: dishName,
+      price: dishPrice
+    }
+    return this._courses[courseName].push(dish);
+    },
+    getRandomDishFromCourse(courseName) {
+    const dishes = this._courses[courseName]
+    const randomIndex = Math.floor(Math.random() * dishes.length);
     
     }
+    
+    };
